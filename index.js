60bd7b41926354adaffbe07b481d3727d6ee64e5
anthony <acorrea4035@gmail.com>
2022-04-10 21:20:26 -0500

    day 3 finished

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
 // inside index.js
 require('dotenv').config();
 
-const PORT = 3000;
-const express = require('express');
+require('dotenv').config();
+const express = require("express");
 const server = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const apiRouter = require("./api");
 
-const morgan = require('morgan');
-server.use(morgan('dev'));
-
-server.use(express.json())
-
-const apiRouter = require('./api');
-server.use('/api', apiRouter);
+server.use(morgan("dev"));
+server.use(bodyParser.json());
+server.use(express.json());
+server.use("/api", apiRouter);
 
 
 const { client } = require('./db');
 client.connect();
-
-server.listen(PORT, () => {
-    console.log('The server is up on port', PORT)
-});
 
 server.use((req, res, next) => {
     console.log("<____Body Logger START____>");
@@ -28,4 +24,18 @@ server.use((req, res, next) => {
 
     next();
 });
+
+server.get('/background/:color', (req, res, next) => {
+    res.send(`
+      <body style="background: ${ req.params.color };">
+        <h1>Hello World</h1>
+      </body>
+    `);
+  });
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log('The server is up on port', PORT)
+});
+
 
